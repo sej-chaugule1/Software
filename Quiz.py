e50a15b98e7558a3c98df6c4a42f93a8a4503d8d
sej-chaugule1 <sejal.chaugule@education.nsw.gov.au>
2024-05-24 09:23:59 +1000

    Deleted the files which had the other attempts for my code and began making edits to my code

--- Quiz.py
+++ Quiz.py
@@ -3,25 +3,26 @@ from tkinter import *
 from tkinter import messagebox, ttk
 from Quiz_data import Quiz_data
 
-def start_game():
-    pass
-
 root = tk.Tk()
 root.title("Identifying time quiz")
 root.geometry("600x600")
-root.configure(bg = "sky blue")
+
+def start_game():
+    label.place_forget()  # hides the label
+    start_button.place_forget()  # hides the start button
+    quit_button.place_forget()  # hides the quit button
+    show_question()
 
 label = tk.Label(root, text="Identifying time quiz", 
                 font=("Arial", 18), 
                 fg="black",
                 bg="lightblue", 
-                bg="white", 
                 padx = 20,
                 pady = 10,
                 borderwidth = 2)
 label.place(x = 170, y = 10)
 
-start_button = tk.Button(root, text="Start Quiz", 
+start_button = tk.Button(root, text="Start", 
                          command=start_game, 
                          width=20, 
                          height=2, 
@@ -29,43 +30,51 @@ start_button = tk.Button(root, text="Start Quiz",
                          borderwidth = 2)
 start_button.place(x = 215, y = 380)
 
-exit_button = tk.Button(root, text="Quit", 
+quit_button = tk.Button(root, text="Quit", 
                         command=root.quit, 
                         width=10, 
                         height=2, 
                         bg = "lightblue",
                         borderwidth = 2)
-exit_button.place(x = 250, y = 450)
+quit_button.place(x = 250, y = 450)
 
-def start_quiz(window):
-    window.destroy()
+
+def skip_question():
+    global current_question
+    question = Quiz_data[current_question]
+    current_question += 1
+
+    if current_question < len(Quiz_data):
+        feedback_label.config(text="The answer is {}".format(question["answer"]), foreground="blue")
+    next_button.config(state="normal")
 
 def show_question():
     question = Quiz_data[current_question]
-    qs_label.config(text=question["question"])
+    question_label.config(text=question["question"])
 
     choices = question["choices"]
-    for i in range(4):
-        choice_btns[i].config(text=choices[i], state="normal")
+    for i in range(4): # 4 options
+        choice_buttons[i].config(text=choices[i], state="normal") # 'normal' meaning the button is not disabled
 
     feedback_label.config(text="")
-    next_btn.config(state="disabled")
+    next_button.config(state="disabled") # the user is unable to interact with the button
 
 def check_answer(choice):
     question = Quiz_data[current_question]
-    selected_choice = choice_btns[choice].cget("text")
+    selected_choice = choice_buttons[choice].cget("text")
 
     if selected_choice == question["answer"]:
         global score
         score += 1
-        score_label.config(text="Score: {}/{}".format(score, len(Quiz_data)))
+        score_label.config(text="Score: {}/{}".format(score, len(Quiz_data))) # the format which scores are displayed in
+
         feedback_label.config(text="Correct! Well done.", foreground="green")
     else:
-        feedback_label.config(text="Incorrect! The answer is", foreground="red")
+        feedback_label.config(text="Incorrect! The answer is {}".format(question["answer"]), foreground="red") # tells the user the correct answer if they got it incorrect
     
-    for button in choice_btns:
+    for button in choice_buttons:
         button.config(state="disabled")
-    next_btn.config(state="normal")
+    next_button.config(state="normal")
 
 def next_question():
     global current_question
@@ -73,31 +82,40 @@ def next_question():
 
     if current_question < len(Quiz_data):
         show_question()
-    else:
-        messagebox.showinfo("Quiz Completed",
-                            "Quiz Completed! Final score: {}/{}".format(score, len(Quiz_data)))
-        root.destroy()
+    else: 
+        show_score_window() # if there are no questions left the quiz will end with displaying the users score
+
+def show_score_window():
+    score_window = tk.Toplevel(root)
+    score_window.title("Total score")
+    score_window.geometry("600x600")
+
+# the 'n' allows the text to go to the next line
+    score_label = tk.Label(score_window, text="Congratulations!\nFinal score: {}/{}".format(score, len(Quiz_data)), font=("Arial", 14))
+    score_label.pack(pady=20, padx=40)
+
+    quit_button = tk.Button(score_window, text="Quit", command=root.quit, width=10, height=2, bg="lightblue")
+    quit_button.pack(padx = 100)
 
 root = tk.Tk()
 root.title("Identifying time quiz")
 root.geometry("600x600")
 
-qs_label = ttk.Label(
+question_label = ttk.Label(
     root,
     anchor="center",
-    wraplength=500,
     padding=10
 )
-qs_label.pack(pady=10)
+question_label.pack(pady=10)
 
-choice_btns = []
+choice_buttons = []
 for i in range(4):
     button = ttk.Button(
         root,
         command=lambda i=i: check_answer(i)
     )
     button.pack(pady=5)
-    choice_btns.append(button)
+    choice_buttons.append(button)
 
 feedback_label = ttk.Label(
     root,
@@ -116,13 +134,20 @@ score_label = ttk.Label(
 )
 score_label.pack(pady=10)
 
-next_btn = ttk.Button(
+next_button = ttk.Button(
     root,
     text="Next",
     command=next_question,
     state="disabled"
 )
-next_btn.pack(pady=10)
+next_button.pack(pady=10)
+
+skip_button = ttk.Button(
+    root, 
+    text="Skip", 
+    command=skip_question
+)
+skip_button.pack (pady=15)
 
 current_question = 0
 
