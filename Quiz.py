aa06de9e2a28ae5d3072410451865096cb0760f9
sej-chaugule1 <sejal.chaugule@education.nsw.gov.au>
2024-05-30 14:20:11 +1000

    Removed unnessecary files, changed the sizing of my images and renamed my quiz file.

--- Quiz.py
+++ Quiz.py
@@ -1,15 +1,79 @@
 import tkinter as tk
-from tkinter import *
-from tkinter import messagebox, ttk
-from Quiz_questions import Quiz_questions
+from tkinter import ttk
 from PIL import ImageTk, Image
 from tkinter import PhotoImage
 
+#Questions, options and answer for my quiz
+Quiz_questions = [
+    {
+    "question": "When the big hand is on 12 and the small hand is on 7,\nwhat is the time?",
+    "choices": ["A. 5:30", "B. 7:00", "C. 12:15", "D. 8:45"],
+    "answer": "B. 7:00"
+},
+    
+{ 
+    "question": "At 9:20 PM, what time is displayed on\na digital clock?",
+    "choices": ["A. 18:45", "B. 04:50", "C. 00:00", "D. 21:20"],
+    "answer": "D. 21:20"
+},
+    
+{
+    "question": "When both hands are pointing at 12,\nwhat is the time?",
+    "choices": ["A. 7:30", "B. 11:55", "C. 12:00", "D. 1:25"],
+    "answer": "C. 12:00"
+},
+    
+{
+    "question": "If it is midnight. What time is displayed \non a digital clock?",
+    "choices": ["A. 00:00", "B. 22:25", "C. 05:05", "D. 13:35"],
+    "answer": "A. 00:00"
+},
+
+{
+    "question": "What time does the analogue clock show when the \nbig hand is on 9 and the small hand is almost on 1?",
+    "choices": ["A. 8:15", "B. 12:45", "C. 9:10", "D. 4:00"],
+    "answer": "B. 12:45"
+},
+
+{
+    "question": "If a digital clock displays 01:25, \nwhat time is it?",
+    "choices": ["A. 7:30", "B. 11:55", "C. 2:10", "D. 1:25"],
+    "answer": "D. 1:25"
+},
+
+{
+    "question": "When the small hand is pointing directly west and \nthe big hand is pointing directly east, \nwhat time is on the analogue clock?",
+    "choices": ["A. 5:30", "B. 3:45", "C. 9:15", "D. 8:45"],
+    "answer": "C. 9:15"
+},
+
+{
+    "question": "If the time is 11:20 at night, \nhow would a digital clock display this time",
+    "choices": ["A. 07:00", "B. 23:20", "C. 14:10", "D. 13:20"],
+    "answer": "B. 23:20"
+},
+
+{
+    "question": "What time is it when both hands \nare on 6 on an analogue clock?",
+    "choices": ["A. 6:30", "B. 4:00", "C. 10:45", "D. 12:20"],
+    "answer": "A. 6:30"
+},
+
+{
+    "question": "If a digital clock shows 19:35, \nwhat time is it?",
+    "choices": ["A. 4:50", "B. 10:25", "C. 5:40", "D. 7:35"],
+    "answer": "D. 7:35"
+}
+]
+
 root = tk.Tk()
-root.title("Identifying time quiz")
+root.title("Digital and analogue time quiz")
 root.geometry("600x600")
 root.configure(bg="lightblue")
 
+font_family_1 = "Comic Sans MS"
+
+#Variables for the quiz page
 def second_page():
     global current_question
     global choice_buttons
@@ -20,91 +84,41 @@ def second_page():
     global next_button
     global skip_button
 
-    root = tk.Tk()
-    root.title("Identifying time quiz")
-    root.geometry("600x600")
-    root.configure(bg="lightblue")
+    question_window = tk.Toplevel(root)
+    question_window.title("Quiz")
+    question_window.geometry("600x600")
+    question_window.configure(bg="lightblue")
 
-    question_label = ttk.Label(root, anchor="center", padding=10)
+    question_label = tk.Label(question_window, text="", bg="white")
     question_label.pack(pady=10)
 
-    choice_buttons = ["choices"]
+    choice_buttons = []
     for i in range(4):
-        button = ttk.Button(root, command=lambda i=i: check_answer(i))
-        button.pack(pady=5)
+        button = ttk.Button(question_window, command=lambda i=i: check_answer(i))
+        button.pack(pady=7)
         choice_buttons.append(button)
 
-    feedback_label = ttk.Label(root, anchor="center", padding=10)
+    feedback_label = tk.Label(question_window, text="", bg="lightblue")
     feedback_label.pack(pady=10)
 
     score = 0
-
-    score_label = tk.Label(root, text="Score: 0/{}".format(len(Quiz_questions), bg = "lightblue"), anchor="center", font=("Comic Sans MS", 15)) 
+    score_label = tk.Label(question_window, text="Score: 0/10".format(len(Quiz_questions)), font=("Comic Sans MS", 15), bg="lightblue")
     score_label.pack(pady=10)
 
-    next_button = ttk.Button(root, text="Next", command=next_question, state="disabled")
+    next_button = ttk.Button(question_window, text="Next", command=next_question, state="disabled")
     next_button.pack(pady=10)
 
-    skip_button = ttk.Button(root, text="Skip", command=skip_question)
+    skip_button = ttk.Button(question_window, text="Skip", command=skip_question)
     skip_button.pack(pady=15)
+
+    image_frame2 = tk.Frame(root, bg="lightblue")
+    image_frame2.place(x=180, y=140)
 
     current_question = 0
 
     show_question()
 
-font_family_1 = "Comic Sans MS"
-    
-label = tk.Label(root, text="Identifying time quiz", 
-                font=(font_family_1, 17),
-                fg="black",
-                bg="white", 
-                padx = 20,
-                pady = 20,
-                borderwidth = 2)
-label.place(x = 160, y = 15)
-
-image_frame = tk.Frame(root, bg="lightblue")
-image_frame.place(x=180, y=140)
-
-image_file = PhotoImage(file="Home.png")
-image_file = image_file.subsample(2,2)
-image = tk.Label(image_frame, image=image_file)
-image.pack(anchor="center")
-
-start_button = tk.Button(root, text="Start", 
-                         command=second_page,
-                         font=(font_family_1, 14),
-                         width=12, 
-                         height=1, 
-                         bg = "sky blue", 
-                         borderwidth = 1)
-start_button.place(x = 220, y = 420)
-
-quit_button = tk.Button(root, text="Quit", 
-                        command=root.quit, 
-                        font=(font_family_1, 14),
-                        width=10, 
-                        height=1, 
-                        bg = "sky blue",
-                        borderwidth = 1)
-quit_button.place(x = 230, y = 490)
-
-def next_question():
-    global current_question
-    current_question +=1
-
-    if current_question < len(Quiz_questions):
-        show_question()
-        skip_button.config(state="normal") #allows the skip button to be normal on the next question
-    else: 
-        show_score_window() #if there are no questions left the quiz will end with displaying the users score
-
-feedback_label = ttk.Label(root, anchor="center", padding=10)
-feedback_label.pack(pady=10)
-
-next_button = ttk.Button(root, text="Next", command=next_question, state="disabled")
-next_button.pack(pady=10)
-
+#Code to show the question when the start button is clicked
 def show_question():
     global current_question
     global question_label
@@ -113,18 +127,18 @@ def show_question():
     global skip_button
 
     question = Quiz_questions[current_question]
-    question_label.config(text=question["question"])
-    question_number = current_question + 1 #adds 1 to every question as it passes
-
-    question_label.config(text=f"Question {question_number} \n{question['question']}", font=("Comic Sans MS", 16), justify = "center") #displays the question number and the question
+    question_label.config(text=question["question"], bg="white")
+    question_number = current_question + 1
+    question_label.config(text=f"Question {question_number}:\n {question['question']}", font=("Comic Sans MS", 15), justify = "center", bg="white")
 
     choices = question["choices"]
-    for i in range(4): # 4 options
-        choice_buttons[i].config(text="choices"[i], state="normal") #'normal' meaning the button is not disabled
-    
-    feedback_label.config(text="")
-    next_button.config(state="disabled") # the user is unable to interact with the button
+    for i in range(4):
+        choice_buttons[i].config(text=choices[i], state="normal")
 
+    feedback_label.config(text="", bg="lightblue")
+    next_button.config(state="disabled")
+
+#Subprogram to check the option clicked by user
 def check_answer(choice):
     global score
     global feedback_label
@@ -134,51 +148,107 @@ def check_answer(choice):
     question = Quiz_questions[current_question]
     selected_choice = choice_buttons[choice].cget("text")
 
-    score_label = tk.Label(root, text="Score: 0/{}".format(len(Quiz_questions), bg = "lightblue"), anchor="center", font=("Comic Sans MS", 15)) 
-    score_label.pack(pady=10)
-
-    skip_button = ttk.Button(root, text="Skip", command=skip_question)
-    skip_button.pack(pady=15)
-
     if selected_choice == question["answer"]:
         global score
-        score += 1
-        score_label.config(text="Score: {}/10".format(score, len(Quiz_questions))) # the format which scores are displayed in
-        feedback_label.config(text="Correct! Well done.", foreground="green", font=("Comic Sans MS", 12))
+        score += 1 #adds 1 point to the score when user answers the question correctly
+        score_label.config(text="Score: {}/10".format(score, len(Quiz_questions)))
+        feedback_label.config(text="Correct! Well done.", foreground="green", font=("Comic Sans MS", 12), bg="lightblue") #text displayed when the user answers correctly
     else:
-        feedback_label.config(text="Incorrect! The answer is {}".format(question["answer"]), foreground="red", font=("Comic Sans MS", 12)) # tells the user the correct answer if they got it incorrect
-    
-    for button in choice_buttons:
-        button.config(state="disabled")
-        skip_button.config(state="disabled") #disables the skip button after the option is selected
-    next_button.config(state="normal")
+        feedback_label.config(text="Incorrect! The answer is {}".format(question["answer"]), foreground="red", font=("Comic Sans MS", 12), bg="lightblue")#text displayed when user answers incorrectly
 
+    for button in choice_buttons:
+        button.config(state="disabled") #disables the option buttons
+        skip_button.config(state="disabled") #disables the skip button 
+    next_button.config(state="normal") 
+
+#Button to skip questions and the feedback that will be given when clicked
 def skip_question():
     global current_question
+    global feedback_label
+    global next_button
+    global Quiz_questions
+
     question = Quiz_questions[current_question]
 
     if current_question < len(Quiz_questions):
         feedback_label.config(text="The answer is {}".format(question["answer"]), foreground="blue", font=("Comic Sans MS", 12))
-    next_button.config(state="normal")
+    next_button.config(state="normal") #allows the next button to be normal in order to go to the next question after skipping
 
-font_family_1 = "Comic Sans MS"
+#Code to call the next question and if at the last question the final score window will open displaying users final score
+def next_question():
+    global current_question
 
+    current_question += 1
+    if current_question < len(Quiz_questions):
+        show_question()
+        skip_button.config(state="normal") #allows the skip button to be normal on the next question
+    else:
+        show_score_window() #if there are no questions left the quiz will end with displaying the users score
+
+font_family_1 = "Comic Sans MS" 
+
+#Final score code:
 def show_score_window():
     score_window = tk.Toplevel(root)
     score_window.title("Total score")
     score_window.geometry("600x600")
     score_window.config(bg="light blue")
 
-    score_title = tk.Label(score_window, text="Quiz Completed!", font= ("font_family_1 17"))
-    score_title.pack(anchor="center")
+    score_title = tk.Label(score_window, text="Quiz Completed!", font= ("Comic Sans MS", 17), bg = "white", borderwidth = 4, padx = 30,
+                pady = 20,)
+    score_title.pack(pady = 10, anchor="center")
 
-# the 'n' allows the text to go to the next line
     score_label = tk.Label(score_window, 
-                           text="Final score: {}/{}".format(score, len(Quiz_questions)),
-                           font=(font_family_1, 17))
-    score_label.pack(pady=20, padx=40)
+                           text="Your score: {}/10".format(score, len(Quiz_questions)),
+                           font=(font_family_1, 20), bg="lightblue")
+    score_label.pack(pady=90, padx=40)
 
-    quit_button = tk.Button(score_window, text="Quit", command=root.quit, width=10, height=2, bg="lightblue")
-    quit_button.pack(pady=50, padx=90)
+    congrats_label = tk.Label(score_window,
+                              text= "Congratulations!",
+                              font= (font_family_1, 20), bg="lightblue")
+    congrats_label.pack(pady=30, padx=40)
+
+    quit_button = tk.Button(score_window, text="Quit", command=root.quit, width=10, height=2, bg="skyblue", font=(font_family_1, 14)) #quits the program when clicked
+    quit_button.pack(pady=30, padx=90)
+
+
+#Starting page code:
+label = tk.Label(root, text="Digital and analogue time quiz", 
+                font=(font_family_1, 19),
+                fg="black",
+                bg="white", 
+                padx = 20,
+                pady = 20,
+                borderwidth = 2)
+label.place(x = 103, y = 15)
+
+#Image displayed on the starting page
+image_frame = tk.Frame(root, bg="lightblue")
+image_frame.place(x=100, y=118)
+
+image_file = PhotoImage(file="Home.png")
+image_file = image_file.subsample(1,1)
+image = tk.Label(image_frame, image=image_file, bg="lightblue")
+image.pack(anchor="center")
+
+#Displays the first question to the user when clicked
+start_button = tk.Button(root, text="Start", 
+                         command=second_page,
+                         font=(font_family_1, 14),
+                         width=12, 
+                         height=1, 
+                         bg = "sky blue", 
+                         borderwidth = 1)
+start_button.place(x = 220, y = 450)
+
+#Closes the whole program when clicked
+quit_button = tk.Button(root, text="Quit", 
+                        command=root.quit, 
+                        font=(font_family_1, 14),
+                        width=10, 
+                        height=1, 
+                        bg = "sky blue",
+                        borderwidth = 1)
+quit_button.place(x = 235, y = 510)
 
 root.mainloop()
